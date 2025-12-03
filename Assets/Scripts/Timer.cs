07a73f4f5bf84c16dcf01edde5127eaa9449bd2a
ANA MARITZA MÁRQUEZ PORRAS <ana_porras@estudante.sesisenai.org.br>
2025-12-03 10:59:03 -0300

    Add Contador de Ox

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,31 @@
+using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI textoTimer;
+    [SerializeField] float tempoRestante;
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (tempoRestante > 0)
+      {
+            tempoRestante -= Time.deltaTime;
+      } 
+      else
+      {
+            tempoRestante = 0;
+            //Game Over();
+            textoTimer.color = Color.red;
+      }  
+
+        int minutos = Mathf.FloorToInt(tempoRestante / 60);
+        int segundos = Mathf.FloorToInt(tempoRestante % 60);
+        textoTimer.text = string.Format("{0:00}:{1:00}",minutos, segundos);
+
+
+
+
+    }
+}
