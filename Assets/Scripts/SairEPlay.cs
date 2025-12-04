930f76c4895e7112ec88981484684bf013c5f34c
ANA MARITZA MÁRQUEZ PORRAS <ana_porras@estudante.sesisenai.org.br>
2025-12-04 10:31:17 -0300

    Add de Menu

---
+++ Assets/Scripts/SairEPlay.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BotoesMenu : MonoBehaviour
+{
+   
+   public void IniciarJogo ()
+   {
+    SceneManager.LoadScene("GameScene");
+   }
+
+public void SairDoJogo()
+{
+    Application.Quit();
+}
+
+public void VoltarProMenu()
+{
+    SceneManager.LoadScene("Menu");
+}
+
+
+
+}
